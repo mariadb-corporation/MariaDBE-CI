c16351cd7d79380540b4f95f31a3a016d6a95336
Timofey Turenko <timofey.turenko@mariadb.com>
2020-02-20 14:46:35 +0200

    try to set locale

--- scripts/es-vm-build.sh
+++ scripts/es-vm-build.sh
@@ -99,9 +99,9 @@ fi
 if [[ ${packager_type} == "apt" ]]
 then
   # DEB-based distro
-  sudo export LANGUAGE=en_US.UTF-8
-  sudo export LANG=en_US.UTF-8
-  sudo export LC_ALL=en_US.UTF-8
+  export LANGUAGE=en_US.UTF-8
+  export LANG=en_US.UTF-8
+  export LC_ALL=en_US.UTF-8
   sudo locale-gen en_US.UTF-8
   export DEBIAN_FRONTEND=noninteractive
   sudo -E apt-get -q -o Dpkg::Options::=--force-confold \
