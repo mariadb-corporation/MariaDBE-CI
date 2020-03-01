9ead40fdcb35a464145c234d8492ac3fbaf54647
Timofey Turenko <timofey.turenko@mariadb.com>
2020-03-01 22:25:07 +0200

    fix debs copying

--- debrpm/create_jenkins_repos.sh
+++ debrpm/create_jenkins_repos.sh
@@ -6,7 +6,7 @@ export script_dir="$(dirname $(readlink -f $0))"
 #jenkins_target=$1
 jenkns_target="10.3/origin/10.3e-abychko/83d402a6a9c0967606508e48894478728cd57fe0"
 
-export direct_in_path=${jenkins_target}/DEB
+export direct_in_path=${jenkins_target}/DEB/*
 export direct_out_path=${jenkins_target}/apt
 
 ${script_dir}/create_deb_repos.sh x
