7639f77b80bf231e73ac93143e2a7f4ffd71d7f9
Timofey Turenko <timofey.turenko@mariadb.com>
2020-03-01 22:27:21 +0200

    fix jenkins paths

--- debrpm/create_jenkins_repos.sh
+++ debrpm/create_jenkins_repos.sh
@@ -3,15 +3,15 @@ set -x
 
 export script_dir="$(dirname $(readlink -f $0))"
 
-#jenkins_target=$1
-jenkns_target="10.3/origin/10.3e-abychko/83d402a6a9c0967606508e48894478728cd57fe0"
+jenkins_target=$1
+#jenkns_target="10.3/origin/10.3e-abychko/83d402a6a9c0967606508e48894478728cd57fe0"
 
-export direct_in_path=${jenkins_target}/DEB/*
-export direct_out_path=${jenkins_target}/apt
+export direct_in_path=/srv/jenkins/${jenkins_target}/DEB/*
+export direct_out_path=/srv/jenkins/${jenkins_target}/apt
 
 ${script_dir}/create_deb_repos.sh x
 
-export direct_in_path=${jenkins_target}/RPMS
-export direct_out_path=${jenkins_target}/yum
+export direct_in_path=/srv/jenkins/${jenkins_target}/RPMS
+export direct_out_path=/srv/jenkins/${jenkins_target}/yum
 
 ${script_dir}/create_rpm_all.sh x
